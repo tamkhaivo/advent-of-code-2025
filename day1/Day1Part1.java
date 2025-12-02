d1459d48648b25425c096d9e30f3d0143e70ce74
i-got-to-eat <khaitamvo@gmail.com>
2025-12-01 16:23:45 -0800

    feat: D1P1 Java Solution

---
+++ day1/Day1Part1.java
@@ -0,0 +1,40 @@
+package day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+    Problem Space; 
+    Find the number of times the dial hits zero when the dial starts at 50;
+
+    Approach 1: Sequential Iteration O(n)
+
+*/
+public class Day1Part1 {
+    static String filePath = "day1/day1.txt";
+    static int dialPosition = 50;
+    static int count = 0;
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int steps = Integer.parseInt(line.substring(1));
+                if (line.startsWith("L")) {
+                    dialPosition -= steps;
+                } else {
+                    dialPosition += steps;
+                }
+                dialPosition %= 100;
+                if (dialPosition == 0) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+
+    }
+}
