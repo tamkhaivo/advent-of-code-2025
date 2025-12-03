d63a186e8541dfa3840d6b926ddd068d9705512f
i-got-to-eat <khaitamvo@gmail.com>
2025-12-02 22:27:02 -0800

    feat: day3part1 solution

---
+++ day3/day3part1.java
@@ -0,0 +1,47 @@
+package day3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class day3part1 {
+    private static final String FILE_PATH = "day3/day3.txt";
+
+    public static void main(String[] args) {
+        long maxPower = solve(FILE_PATH);
+        System.out.println(maxPower);
+    }
+
+    public static long solve(String filePath) {
+
+        int leftNumberIdx = 0, rightNumberIdx = 1;
+        // first highest digit and second highest digit
+        long sum = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char[] values = line.toCharArray();
+                int maxValue = 0;
+                for (int numIdx = 0; numIdx < values.length - 1; numIdx++) {
+                    if (values[numIdx] - '0' > values[leftNumberIdx] - '0') {
+                        leftNumberIdx = numIdx;
+                    }
+                    rightNumberIdx = numIdx + 1;
+                    maxValue = Math.max(maxValue, (values[leftNumberIdx] - '0') * 10 + (values[rightNumberIdx] - '0'));
+                }
+                System.out.println(maxValue + " " + leftNumberIdx + " " + rightNumberIdx);
+                sum += maxValue;
+                leftNumberIdx = 0;
+                rightNumberIdx = 1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+        return sum;
+    }
+}
